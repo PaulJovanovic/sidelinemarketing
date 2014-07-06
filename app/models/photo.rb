69fa7ccba7e6bd605a30b840f823aeac85a5ee19
Paul Jovanovic <paulrealsane@gmail.com>
2014-07-06 11:49:03 -0700

    Changes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < Asset
-  has_attached_file :attachment, :styles => { :small => "148x99#", :medium => "632x422#" }
+  has_attached_file :attachment, :styles => { :small => "148", :medium => "632" }
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
