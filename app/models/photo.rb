200614e45ae6afad9753d89d75773527778d30a5
PaulJovanovic <paulrealsane@gmail.com>
2014-09-16 19:20:40 -0700

    Thumbnails for galleries

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < Asset
-  has_attached_file :attachment, :styles => { :small => "148", :medium => "632" }
+  has_attached_file :attachment, :styles => { :thumbnail => "283x211#", :small => "148", :medium => "632" }
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
